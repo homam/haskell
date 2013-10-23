3348fbc6a052085f53b909d584649dfe1c3b3109
Homam <bluesnowball@gmail.com>
2013-10-23 14:23:26 +0400

    ranges with algebric types

--- ranges.hs
+++ ranges.hs
@@ -1,40 +1,33 @@
-data Range = Range Int Int
+data Range a = Range a a deriving (Read)
 
-range :: Int -> Int -> Range
+range :: Ord a => a -> a -> Range a
 range x y = Range (min x y) (max x y)
 
-instance Show Range where
+instance Show a => Show (Range a) where
 	show (Range x1 x2) = "(" ++ (show x1) ++ ", " ++ (show x2) ++ ")"
 
 
-instance Eq Range where
+instance Eq a => Eq (Range a) where
 	(Range x1 x2) == (Range y1 y2) = x1 == y1 && x2 == y2
 
-instance Ord Range where
+instance Ord a => Ord (Range a) where
 	(Range x1 x2) `compare` (Range y1 y2) = x1 `compare` y1
 
 
-lenR :: Range -> Int
+lenR :: Num a => Range a -> a
 lenR (Range x1 x2) = x2 - x1
 
 
---intersection :: Range -> Range -> Int
---intersection r1 r2 = _intersection (min r1 r2) (max r1 r2)
---	where _intersection (Range x1 x2) (Range y1 y2) = (y1 - x2)/(y2 - x1)
-
-
-
-intersection :: Range -> Range -> Range
-
+intersection :: (Num a, Ord a) => Range a -> Range a -> Range a
 intersection r1 r2 = _intersection (min r1 r2) (max r1 r2)
 	where _intersection (Range x1 x2) (Range y1 y2)
 		| x2 < y1 = Range 0 0
 		| otherwise = Range (min y1 x2) (max y1 x2)
 
 
-union :: Range -> Range -> Range
+union :: (Ord a) => Range a -> Range a -> Range a
 union (Range x1 x2) (Range y1 y2) = Range (min x1 y1) (max x2 y2)
 
 
-intersectionFraction :: Fractional a => Range -> Range -> a
-intersectionFraction r1 r2 = ((fromIntegral . lenR) $  intersection r1 r2) / ((fromIntegral . lenR) $  union r1 r2)
+intersectionFraction :: (Fractional a, Ord a) => Range a -> Range a -> a
+intersectionFraction r1 r2 = (lenR $  intersection r1 r2) / (lenR $ union r1 r2)
