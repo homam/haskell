5c24a04177515aa22ffafca8866cb819255042e3
Homam <bluesnowball@gmail.com>
2013-10-27 23:06:59 +0400

    multi range implemented

--- diag/test.hs
+++ diag/test.hs
@@ -1,6 +1,6 @@
 
 {-# XNoMonomorphismRestriction #-}
-{-# LANGUAGE  CPP #-} -- DeriveDataTypeable,
+{-# LANGUAGE DeriveDataTypeable, CPP #-} -- DeriveDataTypeable,
 
 import System.Environment  (getArgs, getProgName)
 import Diagrams.Prelude hiding (width, height, interval)
@@ -20,16 +20,16 @@ type DRange = Range Double
 
 
 format2 :: Double -> String
-format2 n = show ((fromIntegral $ round $ (n * 10000))/100)
+format2 n = show $ fromIntegral(round (n * 10000))/100
 
 formatPerc2 :: Double -> String
-formatPerc2 = (++"%") . format2 --(++"%") $ show ((fromIntegral $ round $ (n * 10000))/100)
+formatPerc2 = (++"%") . format2 
 
-format2R :: Range Double -> String
+format2R :: DRange -> String
 format2R (Range rx ry) = "(" ++ format2 rx ++ " .. " ++ format2 ry ++ ")"
 
-
-
+-- makes the border transparent
+lcTransparent :: QDiagram SVG R2 Any -> QDiagram SVG R2 Any
 lcTransparent = lcA (black `withOpacity` 0.0)
 
 tick :: Double -> QDiagram SVG R2 Any
@@ -42,31 +42,18 @@ tick v =
 -- drawRangeRect is indipendent of position
 drawRangeRect :: Double -> String -> Double -> QDiagram SVG R2 Any 
 drawRangeRect height label width =
-	(rect width height) # fcA (purple `withOpacity` 0.5)
+	rect width height # fcA (purple `withOpacity` 0.5)
 	<>
 	(text label # scale 0.1)
 
 drawRange :: Double -> String -> Range Double -> QDiagram SVG R2 Any
-drawRange height label r@(Range x1 x2) =
+drawRange height label (Range x1 x2) =
 	position [(p2 ((x1+x2)/2, 0), drawRangeRect height label (x2-x1))]
 
 
 -- layerHeight = 1 / numebr of vertically stacked diagrams 
 
-layerHeight = 0.2
-
-stackR g = (rect 2.1 layerHeight # lcA (black `withOpacity` 0.1)) <> g
-
-rangeA :: DRange
-rangeA = prob 0.035 2587
-
-rangeB :: DRange
-rangeB = prob 0.029 2687
-
-normalizedXY :: (DRange, DRange)
-normalizedXY = normalizeRs (rangeA, rangeB)
-
-
+stackR layerHeight g = rect 2.1 layerHeight # lcA (black `withOpacity` 0.1) <> g
 	
 axisTickLabel :: Double -> Double -> Double -> Double
 axisTickLabel minx maxx x = ((x1+y2)/2)+(x*(y2-x1)/2)
@@ -74,29 +61,31 @@ axisTickLabel minx maxx x = ((x1+y2)/2)+(x*(y2-x1)/2)
 		x1 = minx
 		y2 = maxx
 
+axis :: Double -> Double -> [(P2, QDiagram SVG R2 Any)]
 axis minx maxx = [(p2 (x, 0), tick (axisTickLabel minx maxx x) # scale 0.1) | x <- [(-1),(-0.8).. 1]]
 
 
-
+barsDiagram :: [Range Double] -> QDiagram SVG R2 Any
 barsDiagram ranges = 
-	(
-		(stackR $ position $ axis (min minx1 minx2) (max maxx1 maxx2))
-		===
-		(stackR $ drawRange layerHeight "A" (head normazedRanges))
-		--(stackR $ drawRange layerHeight "A" (fst normalizedXY))
-		-- ===
-		--(stackR $ drawRange layerHeight "B" (snd normalizedXY))
-		-- ===
-		--(stackR $ drawRange layerHeight "C" (snd normalizedXY))
-		-- ===
-		--(stackR $ drawRange layerHeight "D" (snd normalizedXY))
-	)
-	 ===
-	(rect 2.1 1)
+	stackRHeight (position $ axis (min minx1 minx2) (max maxx1 maxx2))
+	=== 
+	foldedRanges
+	=== 
+	rect 2.1 1
 	where
-		sranges = sort ranges
-		minr@(Range minx1 minx2) = head sranges
-		maxr@(Range maxx1 maxx2) = last sranges
-		normazedRanges = normalizeRList ranges
+		layerHeight = 1.0/fromIntegral(length ranges + 1)
+		stackRHeight = stackR layerHeight
 
-main = defaultMain $ barsDiagram $ sort [(prob 0.035 2587), (prob 0.039 2787), (prob 0.042 2882), (prob 0.031 2301)]  -- drawRangeRect "A" 2 --((rect 2 1) <> (text "A" # scale 1 ))
+		-- ranges graphs
+		normazedRanges = normalizeRList ranges
+		stackRHeightRange label = stackRHeight . drawRange layerHeight label
+		foldedRanges = foldl (===) mempty [stackRHeightRange [label] r | (r, label) <- zip normazedRanges ['A'..'Z']]
+
+		-- extends of the graph
+		sranges = sort ranges
+		(Range minx1 minx2) = head sranges
+		(Range maxx1 maxx2) = last sranges
+
+
+main :: IO ()
+main = defaultMain $ barsDiagram [prob 0.035 2587, prob 0.034 2787, prob 0.042 2882, prob 0.031 2301]  -- drawRangeRect "A" 2 --((rect 2 1) <> (text "A" # scale 1 ))
