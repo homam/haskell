4098b0c1ac1e2e57bb74e2f601d8f54a62fe26ac
Homam <bluesnowball@gmail.com>
2013-11-05 15:29:32 +0400

    normal . instead of $

--- functors/io.hs
+++ functors/io.hs
@@ -1,6 +1,11 @@
+import Control.Applicative
 
-
+main :: IO ()
 main = do 
 	line <- fmap reverse getLine
 	--let line' = reverse line
 	putStrLn line
+
+
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
