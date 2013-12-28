ed3b4e751de4b87e7b5f95357747f8925dfb9d02
Homam <bluesnowball@gmail.com>
2013-12-28 14:00:38 +0400

    type declerations

--- functors/fact.hs
+++ functors/fact.hs
@@ -6,16 +6,8 @@ fact 0 = 1
 fact n = n * fact (n-1) 
 
 
-inv n = 1 / n
 
-
-
-y p n x = (fromIntegral y1) where  -- 1 / (fromIntegral y1)
-		y1 = ((fact . truncate $ x)*(fact . truncate $ n-x)) `div` ((fact . truncate $ n) `div` (truncate . inv $ ( p**x * (1-p)**(n-x) ))) 
-
-
-
-choose n k = (fact n) `div` ( (fact k) * (fact (n-k)) )
+choose n k = fact n `div` ( fact k * fact ( n - k ) )
 
 cross a b = 1 / fromIntegral (b `div` a)
 
@@ -23,22 +15,35 @@ times a 1 = a
 times a !n = (a +) $! (times a $! (n-1))
 
 
--- y' p n x = (choose (truncate n) (truncate x)) * (p**x) * (1-p)**(n-x)
+times' v _ 1 = v
+times' v a n = seq b (seq n1 (times' b a n1)) where
+	b = v + a 
+	n1 = n - 1
 
-y' p n x = 1 / (fromIntegral up) where
-	up = (truncate ( 1 / ( (p**x) * (1-p)**(n-x) ) ) * (fact . truncate $ x) * (fact . truncate $ n-x) )  `div` (fact . truncate $ n)
-
-
-y'' p n x =  1 / (fromIntegral up) where
-	up = (truncate ( 1 / ( (p**(fromIntegral x)) * (1-p)**((fromIntegral n)-(fromIntegral x)) ) ) * (fact x) * (fact $ n-x) )  `div` (fact n)
 
 
 
 
 -- this one is the done to modify
-y''' s n x = 1 / fromIntegral (down `div` up)  where -- `div` down where
-	up = (fact n) * (s^x) * ((n-s)^(n-x))
-	down = (fact x) * (fact $ n-x) * (n^x) * (n^(n-x))
+
+binomial :: (Fractional b, Integral a) => a -> a -> a -> b
+binomial s n x = 1 / fromIntegral (down `div` up)  where -- `div` down where
+	up = fact n * (s^x) * ((n-s)^(n-x))
+	down = fact x * fact (n -x ) * (n^x) * (n^(n-x))
+
+
+normal :: Floating a => a -> a -> a -> a
+normal sigma mu x = c * exp(eup / edown) where
+	c = 1/(sigma*sqrt(2 * pi))
+	eup = (x - mu)**2
+	edown = -2 * (sigma**2)
+
+binomialNormal :: (Floating b, Integral a) => a -> a -> b -> b
+binomialNormal s n = normal sigma mu where
+	[n', s'] = fromIntegral <$> [n, s]
+	p = s'/n'
+	mu = n' * p
+	sigma = sqrt $ n' * p * (1 - p)
 
 
 
@@ -61,31 +66,31 @@ y''' s n x = 1 / fromIntegral (down `div` up)  where -- `div` down where
 
 
 -- in haskell:
--- ( sum [ abs ((y''' 85 155 i) - (y''' 80 155 i)) | i <- [0..155]] ) / 2
+-- ( sum [ abs ((y 85 155 i) - (y 80 155 i)) | i <- [0..155]] ) / 2
 
-conf n a b = ( sum [ abs ((y''' a n i) - (y''' b n i)) | i <- [0..n]] ) / 2
+binomialConf :: (Fractional b, Integral a) => a -> a -> a -> b
+binomialConf n a b = sum [ abs (binomial a n i - binomial b n i) | i <- [0..n]] / 2
 
-result (na, a) (nb, b) = (aConv, bConv, diff, conf (floor n) (floor a'') (floor b'')) where
-	[a', b', na', nb'] = fromIntegral <$> [a,b,na,nb]
-	n = max na' nb'
-	ra = n / na'
-	rb = n / nb'
-	a'' = a' * ra
-	b'' = b' * rb
-	aConv = a' / na'
-	bConv = b' / nb'
-	diff = (abs $ aConv - bConv) / (min aConv bConv)
+binomialResult :: (Integer, Integer) -> (Integer, Integer) -> (Double, Double, Double, Double)
+binomialResult = result binomialConf
 	
 
 
 
-
-
-binomialNormalConf n a b =  ( sum [ abs ((binomialNormal a' n' (fromIntegral i)) - (binomialNormal b' n' (fromIntegral i))) | i <- [0..n]] ) / 2
+binomialNormalConf :: (Floating b, Integral a) => a -> a -> a -> b
+binomialNormalConf n a b =  sum [ abs (binomialNormal a' n' (fromIntegral i) - binomialNormal b' n' (fromIntegral i)) | i <- [0..n]] / 2
 	where
 		[n', a', b'] = fromIntegral <$> [n, a, b]
 
-binomialResult (na, a) (nb, b) = (aConv, bConv, diff, binomialNormalConf (floor n) (floor a'') (floor b'')) where
+binomialNormalResult :: (Integer, Integer) -> (Integer, Integer) -> (Double, Double, Double, Double)
+binomialNormalResult = result binomialNormalConf
+
+
+
+
+
+result :: (Integral t, Integral b, Integral b1, Integral b2, RealFrac a) => (b -> b1 -> b2 -> t1) -> (t, t) -> (t, t) -> (a, a, a, t1)
+result confFunc (na, a) (nb, b) = (aConv, bConv, diff, confFunc (floor n) (floor a'') (floor b'')) where
 	[a', b', na', nb'] = fromIntegral <$> [a,b,na,nb]
 	n = max na' nb'
 	ra = n / na'
@@ -94,29 +99,4 @@ binomialResult (na, a) (nb, b) = (aConv, bConv, diff, binomialNormalConf (floor 
 	b'' = b' * rb
 	aConv = a' / na'
 	bConv = b' / nb'
-	diff = (abs $ aConv - bConv) / (min aConv bConv)
-
-
-
-normal sigma mu x = c * exp(eup / edown) where
-	c = 1/(sigma*sqrt(2 * pi))
-	eup = (x - mu)**2
-	edown = -2 * (sigma**2)
-
-binomialNormal s n = normal sigma mu where
-	[n', s'] = fromIntegral <$> [n, s]
-	p = s'/n'
-	mu = n' * p
-	sigma = sqrt $ n' * p * (1 - p)
-
-
-
-
-
-
-
-y'''' s n x =  (fromIntegral up) / (fromIntegral down) where -- (down `div` up)  where -- 1 / fromIntegral (down `div` up)  where -- `div` down where
-	up = (fact n) * (s^x) * ((n-s)^(n-x))
-	down = (fact x) * (fact $ n-x) * (n^x) * (n^(n-x))
-
-
+	diff = abs (aConv - bConv) / min aConv bConv
